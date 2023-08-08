d84fd4c45fc7c26d9c03186d158f1df4a1698fc3
vishakha-jft <vishakha.yadav@jellyfishtechnologies.com>
2023-08-08 13:11:56 +0530

    first Commit

---
+++ src/main/java/com/example/s3_app/S3AppApplication.java
@@ -0,0 +1,13 @@
+package com.example.s3_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3AppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S3AppApplication.class, args);
+    }
+
+}
